df267a912684ee93cc09702392e74bba5638af46
Dmytro Tarasenko <dm.tarasenko97@gmail.com>
2022-09-26 23:50:52 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,27 +1,45 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
 
-const API_KEY = '30168834-4da4897511e2455f9943883b1';
-const BASE_URL = 'https://pixabay.com/api/';
-
 const seachFormEl = document.querySelector('#search-form');
 const galleryEl = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+const API_KEY = '30168834-4da4897511e2455f9943883b1';
+const BASE_URL = 'https://pixabay.com/api/';
+const perPage = 40;
+let limitPage = 1;
+let page = 1;
+let searchValue = '';
 
 seachFormEl.addEventListener('submit', onSeachFormSubmit);
+loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
 
 function onSeachFormSubmit(e) {
   e.preventDefault();
 
-  const searchValue = e.target.elements.searchQuery.value;
+  searchValue = e.target.elements.searchQuery.value;
+  page = 1;
 
-  getPhotos(searchValue)
+  galleryEl.innerHTML = '';
+
+  getPhotos(searchValue, page)
+    .then(renderMarkup)
+    .then(markup => {
+      galleryEl.insertAdjacentHTML('beforeend', markup);
+      loadMoreBtn.classList.add('hidden');
+    });
+}
+
+function onLoadMoreBtnClick(e) {
+  page += 1;
+  getPhotos(searchValue, page)
     .then(renderMarkup)
     .then(markup => {
       galleryEl.insertAdjacentHTML('beforeend', markup);
     });
 }
 
-async function getPhotos(searchValue) {
+async function getPhotos(searchValue, page) {
   try {
     const response = await axios.get(BASE_URL, {
       params: {
@@ -29,15 +47,27 @@ async function getPhotos(searchValue) {
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
+        per_page: `${perPage}`,
+        page: `${page}`,
         q: `${searchValue}`,
       },
     });
+
+    limitPage = response.data.totalHits / perPage;
 
     if (!response.data.hits.length) {
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
+
       return;
+    }
+
+    if (limitPage <= page) {
+      Notiflix.Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+      loadMoreBtn.classList.remove('hidden');
     }
 
     return response.data.hits;
